467aef6c9b7b2ffecc4aa645c4eae071399daa09
HyeongwooIM <hyeongwoo1279@gmail.com>
2023-11-24 15:02:17 +0900

    [Fix]파일네임으로 인한 추적 불가 수정

--- pong-game/components/DropDown/Function/ChatKick.tsx
+++
@@ -1,7 +0,0 @@
-export default function ChatKick() {
-  return (
-    <li>
-      <button>강퇴하기</button>
-    </li>
-  )
-}
--- pong-game/components/DropDown/Function/DirectMsg.tsx
+++
@@ -1,5 +0,0 @@
-export default function DirectMsg() {
-  return (<li>
-  <button>1:1메세지</button>
-  </li>)
-}
