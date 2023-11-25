e962030659a89e12a91c746ba9126ec8c620120e
exceed96 <exceed_96@naver.com>
2023-11-25 13:51:11 +0900

    [Feat] channelType divide logic edit #64

--- pong-game/components/Modal/CreateRoom/CreateRoom.tsx
+++ pong-game/components/Modal/CreateRoom/CreateRoom.tsx
@@ -14,19 +14,33 @@ function CreateChatRoom(): JSX.Element {
   const passwordRef = useRef<HTMLInputElement>(null)
 
   const createChannel = async (channelType, password = null) => {
-    if (!titleRef.current || !titleRef.current.value) {
-      setError('noTitle')
+    if (!titleRef.current?.value) {
+      //title값을 입력안했거나 null일 경우에는 set state "noTitle"로 지정
+      setError('noTitle') //title은 어차피 비워진 상태여서 따로 초기화 안해줘도 된다.
       return
     }
-    if (passwordRef.current.value.length <= 8) {
+
+    if (channelType === 'PROTECTED' && !passwordRef.current.value) {
+      //채널의 타입이 "PROTECTED"이지만 패스워드가 없다면 채널의 타입은 "PUBLIC"
+      channelType = 'PUBLIC'
+    }
+
+    if (
+      channelType === 'PROTECTED' &&
+      passwordRef.current.value.length <= 8 &&
+      passwordRef.current.value
+    ) {
+      //password값이 8자 이하일 경우 set state "lessPassword"로 지정
       setError('lessPassword')
-      passwordRef.current.value = ''
+      passwordRef.current.value = '' //그리고 해당 비밀번호 입력 값을 비워준다.
       return
     }
+
     const datas = {
-      name: titleRef.current.value,
-      channelType: password ? 'PRIVATE' : 'PUBLIC',
-      password: channelType === 'PUBLIC' || 'DM' ? null : password,
+      //보내려는 데이터를 channel 타입에 맞춰서 값을 초기화
+      name: titleRef.current.value, //모든 타입에는 공통적으로 title이 들어간다.
+      channelType: channelType,
+      password: channelType === 'PUBLIC' || 'PRIVATE' ? null : password, //채널의 타입이 PUBLIC이거나 PRIVATE라면 패스워드는 "null"
     }
     try {
       const response = await instance({
@@ -34,10 +48,14 @@ function CreateChatRoom(): JSX.Element {
         url: 'https://localhost:3000/channels',
         data: JSON.stringify(datas),
       })
+      console.log(datas)
       console.log(response)
-      setModalName(null)
-      titleRef.current.value = ''
-      channelType !== 'PRIVATE' ? (passwordRef.current.value = '') : ''
+      if (response.statusText === 'Created') {
+        //채널 생성에 성공한다면 모달창을 꺼주고 채널뷰를 생성한 채널뷰로 바꿔줘야한다.(채널뷰 변경 추가예정)
+        setModalName(null)
+      }
+      titleRef.current.value = '' //기존에 남아있던 값을 비워준다.
+      channelType !== 'PRIVATE' ? (passwordRef.current.value = '') : '' //채널의 타입이 "PRIVATE"가 아니라면 패스워드값을 비워준다.
     } catch (error) {
       console.log('Api Request fail : ', error)
     }
@@ -51,8 +69,6 @@ function CreateChatRoom(): JSX.Element {
         break
       case 'publicOrProtected':
         createChannel('PROTECTED', passwordRef.current.value)
-        break
-      default:
         break
     }
   }
