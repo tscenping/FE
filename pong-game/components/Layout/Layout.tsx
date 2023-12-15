26040a35f8be9dd2601d0ff8dc85ea1da2af5cb0
exceed96 <exceed_96@naver.com>
2023-12-15 13:18:32 +0900

    [Design] block user nickname out of opponent message #92

--- pong-game/components/Layout/Layout.tsx
+++ pong-game/components/Layout/Layout.tsx
@@ -45,6 +45,41 @@ function Layout({ children }: { children: ReactNode }): JSX.Element {
     responseModal.setResponseModalState('로그아웃', '로그아웃 하시겠습니까?', logoutHandler)
   }
 
+  const handler = (t) => {
+    toast.remove(t.id)
+  }
+  useEffect(() => {
+    socket.on('privateAlert', (msg) => {
+      // msg.invitingUserId가 차단된 유저인지 확인
+      const isBlockedUser = allBlocks.some((blockUser) => blockUser.nickname === msg.invitingUserId)
+
+      // 차단된 유저가 아닌 경우에만 toast 로직 실행
+      if (!isBlockedUser) {
+        toast(
+          (t) => (
+            <div className={styles.toastContainer}>
+              <section className={styles.toastMessage}>
+                <strong className={styles.inviteChannel}>{msg.invitingUserId}</strong>
+                <span> 님이 채널에서 초대를 보냈습니다.</span>
+              </section>
+              <section className={styles.toastButton}>
+                <button onClick={() => toast.remove()}>수 락</button>{' '}
+                {/* 채팅 초대 수락 api, 따로 핸들러 함수 만들어서 실행*/}
+                <button onClick={() => handler(t)}>거 절</button>{' '}
+                {/* 채팅 초대 거절 api, 따로 핸들러 함수 만들어서 실행*/}
+              </section>
+            </div>
+          ),
+          { duration: 10000 },
+        )
+      }
+    })
+
+    return () => {
+      socket.off('privateAlert')
+    }
+  }, [socket, allBlocks])
+
   const handleReceiveMessage = (message: messageProps) => {
     const time = new Date()
     const hour = String(time.getHours()).padStart(2, '0')
@@ -70,38 +105,6 @@ function Layout({ children }: { children: ReactNode }): JSX.Element {
       }
     }
   }
-
-  useEffect(() => {
-    socket.on('privateAlert', (msg) => {
-      // msg.invitingUserId가 차단된 유저인지 확인
-      const isBlockedUser = allBlocks.some((blockUser) => blockUser.nickname === msg.invitingUserId)
-
-      // 차단된 유저가 아닌 경우에만 toast 로직 실행
-      if (!isBlockedUser) {
-        toast(
-          (t) => (
-            <div className={styles.toastContainer}>
-              <section className={styles.toastMessage}>
-                <strong className={styles.inviteChannel}>{msg.invitingUserId}</strong>
-                <span> 님이 채널에서 초대를 보냈습니다.</span>
-              </section>
-              <section className={styles.toastButton}>
-                <button onClick={() => toast.remove()}>수 락</button>{' '}
-                {/* 채팅 초대 수락 api, 따로 핸들러 함수 만들어서 실행*/}
-                <button onClick={() => toast.remove(t.id)}>거 절</button>{' '}
-                {/* 채팅 초대 거절 api, 따로 핸들러 함수 만들어서 실행*/}
-              </section>
-            </div>
-          ),
-          { duration: 10000 },
-        )
-      }
-    })
-
-    return () => {
-      socket.off('privateAlert')
-    }
-  }, [socket, allBlocks])
 
   useEffect(() => {
     socket.on('message', handleReceiveMessage)
