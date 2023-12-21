c849c8cfc606c6842b5d209180ed400a253018a6
exceed96 <exceed_96@naver.com>
2023-12-21 13:56:00 +0900

    [Refactor] chat index.tsx file fix with eslint #103

--- pong-game/pages/chat/index.tsx
+++ pong-game/pages/chat/index.tsx
@@ -1,24 +1,7 @@
 import PageTitle from '@/components/UI/PageTitle'
 import ChatMainContents from '@/components/Chat/ChatMainContents'
 
-interface getChannelDataProps {
-  channelId: number
-  name: string
-  channelType: string
-  entered: boolean
-  userCount: string
-}
-
-interface ChatPageProps {
-  totalAll: number
-  totalMe: number
-  totalDm: number
-  allChannel: getChannelDataProps[]
-  meChannel: getChannelDataProps[]
-  dmChannel: getChannelDataProps[]
-}
-
-function ChatPage(props: ChatPageProps) {
+function ChatPage() {
   return (
     <>
       <PageTitle title={'Chatting'} subTitle={'다른유저, 친구와 대화를 나눠보세요.'} />
